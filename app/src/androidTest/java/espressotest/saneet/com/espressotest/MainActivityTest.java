7684268eaa9ce1212e51c21431117044360eb6c6
Saneet <sbonde3@ford.com>
2019-03-03 22:27:07 -0500

    Added UIAutomator tests and config file.

--- app/src/androidTest/java/espressotest/saneet/com/espressotest/MainActivityTest.java
+++ app/src/androidTest/java/espressotest/saneet/com/espressotest/MainActivityTest.java
@@ -17,6 +17,39 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
+    private static final String BASIC_SAMPLE_PACKAGE
+            = "espressotest.saneet.com.espressotest";
+
+    private static final int LAUNCH_TIMEOUT = 5000;
+
+    private static final String STRING_TO_BE_TYPED = "UiAutomator";
+
+    private UiDevice mDevice;
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(getInstrumentation());
+
+        // Start from the home screen
+        mDevice.pressHome();
+
+        // Wait for launcher
+        final String launcherPackage = getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
+
+        // Launch the blueprint app
+        Context context = getApplicationContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);    // Clear out any previous instances
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        mDevice.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)), LAUNCH_TIMEOUT);
+    }
+
     @Rule
     public final ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
 
@@ -29,7 +62,7 @@ public class MainActivityTest {
     @Test
     public void dashboard() {
         onView(withId(R.id.navigation_dashboard)).perform(click());
-        onView(withId(R.id.message)).check(matches(withText(R.string.title_dashboard)));
+        onView(withId(R.id.message)).check(matches(withText("SomeText")));
     }
 
     @Test
